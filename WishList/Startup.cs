d29432b1a860bc1e50dbabee9ffcb58dfa1daecb
V1\BrodyE <enda.brody@version1.com>
2023-02-10 08:55:05 +0000

    FIRST COMMIT

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,7 +16,9 @@ namespace WishList
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            AddMvc
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
