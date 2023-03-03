08db341d394c584e6e09a040d360927d31a43132
V1\BrodyE <enda.brody@version1.com>
2023-03-03 08:49:40 +0000

    Module 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public 
+    }
+}
